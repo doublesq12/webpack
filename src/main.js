4370b5d9c3b44e2a2fab1982b9ba88ac42d14087
doublekou <951513186@qq.com>
2022-06-27 21:22:06 +0800

    加载器 - 处理图片

--- src/main.js
+++ src/main.js
@@ -3,3 +3,8 @@ import './tab.js'
 import $ from 'jquery'
 $('#swiper').css('background', 'pink')
 import './style/index.css'
+import './style/index.less'
+import imgUrl from './assets/1.gif'
+const img = document.createElement("img")
+img.src = imgUrl
+document.body.appendChild(img)
--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,37 @@ module.exports = {
         // 先用 css-loader 让webpack能够识别 css 文件的内容并打包
         // 再用 style-loader 将样式, 把css插入到dom中
         use: [ "style-loader", "css-loader"]
-      }
+      },
+      {
+        test: /\.less$/, // 匹配执行类型的文件
+        // 使用less-loader, 让webpack处理less文件, 内置还会用less翻译less代码成css内容
+        // 执行的顺序 less-loader css-loader style-loader
+        // less-loader 先把less代码转换成css
+        // css-loader 再把css代码转换成webpack 可以识别的js代码
+        // style-loader 在把css代码插入到 dom中
+          use: [ "style-loader", "css-loader", 'less-loader']
+      },
+      { // 图片文件的配置(仅适用于webpack5版本)
+        test: /\.(png|jpg|gif|jpeg)$/i,
+        // type: 'asset', // 在导出一个 data URI 和发送一个单独的文件之间自动选择
+        // 如果你设置的是asset模式
+        // 以8KB大小区分图片文件
+        // 小于8KB的, 把图片文件转base64, 打包进js中
+        // 大于8KB的, 直接把图片文件输出到dist下
+          
+        // type: 'asset/resource' // 发送一个单独的文件并导出 URL
+        // type: 'asset/inline' // 导出一个资源的 data URI
+        type: 'asset',
+        parser: { // 解析器 规则
+          dataUrlCondition: { // dataUrl的情况
+            maxSize: 8 * 1024,
+            // maxSize 限制最大值
+          },
+        },
+        generator: {
+          filename:'[hash:6][ext]'
+        }
+      },
     ]
 }
 }
